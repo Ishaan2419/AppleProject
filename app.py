e1bb3a99e253307fa332b13ab3fb56300ab694e7
Ishaan <ishaanraut02@gmail.com>
2026-03-20 15:08:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import gdown
+import os
+
+# Download model if not present
+if not os.path.exists("fruit_model.keras"):
+    url = "https://drive.google.com/uc?id=106hP_fAg-_sez85rh1NHZ8iTmwDafbYO"
+    gdown.download(url, "fruit_model.keras", quiet=False)
 
 # Load trained model
 model = tf.keras.models.load_model("fruit_model.keras")
